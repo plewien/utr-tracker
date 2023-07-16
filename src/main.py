e5c75c2b7713f69fbd0f917972e85ed18dc1d836
Patrick Lewien <patdanlew@gmail.com>
2023-07-16 16:03:46 +1000

    Only require matplotlib if graphing

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 from config import UTRArgs
-from filesystem import append_utr_data
-from graph import plot_utr_data
 from session import UTRSession
 
 if __name__ == '__main__':
 	args = UTRArgs().parse()
 
 	if args.id or args.name:
+		from filesystem import append_utr_data
 		session = UTRSession().login(args.username, args.password)
 		data = session.get_player_data(args.id, args.name)
 		append_utr_data(args.database, data)
 
 	if args.graph:
+		from graph import plot_utr_data
 		plot_utr_data(args.database)
